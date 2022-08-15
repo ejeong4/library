7909377b6a479d5f08ba71db2104b3d13268252e
ejeong4 <mlej530@gmail.com>
2022-08-15 14:48:01 -0400

    Create add card function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cards = document.querySelector('.cards');
+
 let library = [];
 
 function Book(title, author, pages, read) {
@@ -7,15 +9,27 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function createCard() {
+    library.forEach(Element => {
+        const cardDiv = document.createElement('div');
+        const titleDiv = document.createElement('h1')
+        const infoDiv = document.createElement('h3');
+
+        cardDiv.appendChild(titleDiv);
+        cardDiv.appendChild(infoDiv);
+
+        titleDiv.textContent = `Title: ${Element.title}`;
+        infoDiv.textContent = `Author: ${Element.author} Pages: ${Element.pages} ${Element.read}`;
+
+        cards.appendChild(cardDiv);
+        return cardDiv;
+    })
+};
+
 Book.prototype.addBookToLibrary = function() {
     library.push(this);
 }
 
-Book.prototype.sayBook = function() {
-    console.log(this);
-}
-
 const book = new Book('Harry Potter', 'JK Rowling', '300', 'read');
-book.sayBook();
 book.addBookToLibrary();
-console.log(library);
+createCard();
