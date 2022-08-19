786e67f392c040c377bb2148125bf7b2154823e2
ejeong4 <mlej530@gmail.com>
2022-08-19 17:14:59 -0400

    Create working form and submit btn

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const cards = document.querySelector('.cards');
 const bookBtn = document.querySelector('.new-book');
 const body = document.querySelector('body');
+
 const form = document.querySelector('.form');
 const formClose = document.querySelector('.form-close');
+const formSubmit = document.querySelector('.form-submit');
+
+const formTitle = document.querySelector('#title');
+const formAuthor = document.querySelector('#author');
+const formPages = document.querySelector('#pages');
+const formRead = document.querySelector('#read');
 const formInputs = document.querySelectorAll('.form-input');
 
 let library = [];
@@ -14,46 +21,49 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function createCard() {
-    library.forEach(Element => {
-        const cardDiv = document.createElement('div');
-        const titleDiv = document.createElement('h1')
-        const authorDiv = document.createElement('h3');
-        const pagesDiv = document.createElement('h3');
-        const readDiv = document.createElement('h3');
+Book.prototype.addBookToLibrary = function() {
+    library.push(this);
+}
 
-        cardDiv.classList.add('cardDiv');
-        [titleDiv, authorDiv, pagesDiv, readDiv].forEach(
-            Element => cardDiv.appendChild(Element)
-        );
+function createCard(book) {
+    const cardDiv = document.createElement('div');
+    const titleDiv = document.createElement('h1')
+    const authorDiv = document.createElement('h3');
+    const pagesDiv = document.createElement('h3');
+    const readDiv = document.createElement('h3');
 
-        titleDiv.textContent = `Title: ${Element.title}`;
-        authorDiv.textContent = `Author: ${Element.author}`;
-        pagesDiv.textContent = `Pages: ${Element.pages}`;
-        readDiv.textContent = Element.read;
+    cardDiv.classList.add('cardDiv');
+    [titleDiv, authorDiv, pagesDiv, readDiv].forEach(
+        Element => cardDiv.appendChild(Element)
+    );
 
-        cards.appendChild(cardDiv);
-        return cardDiv;
-    })
-};
+    titleDiv.textContent = `Title: ${book.title}`;
+    authorDiv.textContent = `Author: ${book.author}`;
+    pagesDiv.textContent = `Pages: ${book.pages}`;
+    readDiv.textContent = book.read;
 
-bookBtn.addEventListener('click', () => form.setAttribute('style', 'display: block;'));
+    cards.appendChild(cardDiv);
+    return cardDiv;
+}
 
-formClose.addEventListener('click', () => {
+function formReset() {
     formInputs.forEach((input) => {
         input.value = '';
         input.checked = false;
     });
     form.setAttribute('style', 'display: none;');
-});
-
-Book.prototype.addBookToLibrary = function() {
-    library.push(this);
 }
 
-const book = new Book('Harry Potter', 'JK Rowling', '300', 'read');
-const book1 = new Book('Atomic Habits', 'James Clear', '300', 'read');
-book.addBookToLibrary();
-book1.addBookToLibrary();
+formSubmit.addEventListener('click', () => {
+    let readValue = '';
+    if (formRead.value == 'on') {
+        readValue += 'read';
+    }
+    const book = new Book(formTitle.value, formAuthor.value, formPages.value, readValue);
+    book.addBookToLibrary();
+    createCard(book);
+    formReset();
+});
 
-createCard();
+bookBtn.addEventListener('click', () => form.setAttribute('style', 'display: block;'));
+formClose.addEventListener('click', formReset());
